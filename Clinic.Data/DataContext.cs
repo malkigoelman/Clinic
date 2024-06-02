065bf7a3d2908342a18b977a41d1afab33f2ed97
hitmachut <hitmachut@students.local>
2024-06-02 13:02:22 +0300

    from seminary

--- Clinic.Data/DataContext.cs
+++ Clinic.Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Mirpaha.Entities;
 using System.Diagnostics;
 //using (var reader = new StreamReader("data.csv"))
@@ -11,10 +12,15 @@ namespace Mirpaha
         public DbSet<Client> Clients { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Specialization> Specialization { get; set; }
+        private readonly IConfiguration _configuration;
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=sample1_db");
+            optionsBuilder.UseSqlServer(_configuration["DbConnectionString"]);
             optionsBuilder.LogTo(m=>Debug.WriteLine(m));    
         }
 
